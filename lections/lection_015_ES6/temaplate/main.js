c85d181b04a2e884f7dd9efcb1f639e35e699edd
GeneralGLA-true <artemstarodub@gmail.com>
2023-09-11 21:14:06 +0300

    Update main.js

--- lections/lection_015_ES6/temaplate/main.js
+++ lections/lection_015_ES6/temaplate/main.js
@@ -7,27 +7,27 @@ const fetchAllProducts = async () => {
 async function getAllProducts() {
   const response = await fetchAllProducts();
   const prouducts = response.products;
-
+  
   console.log(prouducts, "prouducts");
 
   const productsTamplate = `<section class="product-item">
   <div class="image-wrapper">
     <img
-      src="https://40.media.tumblr.com/2a46a0ec64f5d1c0dcc8814baf9833f4/tumblr_nj930lpXGB1qif4c6o1_1280.jpg"
+      src="${prouducts[1].images[0]}"
       alt=""
       class="image"
     />
   </div>
   <div class="content-wrapper">
     <div class="title">
-      <h4>Folk-tastic Pennant</h4>
+      <h4> ${prouducts[1].brand}</h4>
     </div>
     <div class="price">
-      $19.9
-      <span class="discount">[відобразити суму з урахуванням знижки]</span> 
+      $ ${prouducts[1].price}
+      <span class="discount">'$ ${prouducts[1].discountPercentage}</span> 
     </div>
 
-    <div class="description">some description</div>
+    <div class="description">${prouducts[1].description}</div>
     <div class="actions">
       <button id="cart"class="button green-solid cart">
           Add to Cart
@@ -36,6 +36,7 @@ async function getAllProducts() {
     </div>
   </div>
 </section>`;
+document.body.innerHTML = productsTamplate
 }
 
 getAllProducts();
